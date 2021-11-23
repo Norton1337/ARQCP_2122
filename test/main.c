0a910ea784c0056c070c7ae6e3d7d5aedc8e6adc
goncalo21r <1190610@isep.ipp.pt>
2021-11-23 14:59:08 +0000

    teste feito

--- test/main.c
+++ test/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "nota.h"
+#include "min_max_notas.h"
+
 int main(){
-
-
+	short quantidade=0;
+	unsigned int notas[10] = {0x0B0F0A10, 0x0C0A0907, 0x0B080A0C, 0x0710080A,
+		 0x0F120E0F, 0x0E110B0C, 0x0B0F0A0B, 0x0A0B0F10, 0x0F101112, 0x090F0A06};
+		 
+	quantidade = calcula_notas(notas,10);
 	
+	printf("\nNota minima da turma: %hhd\n",nota_min);
+	printf("Nota maxima da turma: %hhd\n\n",nota_max);
+	printf("Numero de alterações: %hd\n\n",quantidade);
 	
 	return 0;
 }
--- test/min_max_notas.s
+++ test/min_max_notas.s
@@ -6,9 +6,42 @@
 	nota_max:
 		.byte 0
 
+	.global nota_min
+	.global nota_max
 	
 .section .text
 
+	.global min_max_notas
+	min_max_notas:
+		pushq %rbx
+		movq $0, %rax 	#colocar todos os registos a zeros para evitar erros na comparação
+		movq $0, %rbx
+		movq $0, %rcx
+		movq $0, %rdx
+		
+		movb nota_final(%rip), %bl
+		movb nota_min(%rip), %cl
+		movb nota_max(%rip), %dl
+		
+		cmpb %dl, %bl 
+		jg newMax
+		
+		cmpb %cl, %bl
+		jl newMin
+		
+		jmp end
+		
+	newMin: 			#nota final é menor que a nota minima, logo, editar nota minima e retornar 1
+		movb $1, %al
+		movb %bl, nota_min(%rip)
+		jmp end
 	
-
-	
+	newMax: 			#nota final é maior que a nota maxima, logo, editar nota maxima e retornar 1
+		movb $1, %al
+		movb %bl, nota_max(%rip)
+		cmpb %cl, %bl 	# testar se nota final é menor que a nota minima (não foi testado antes)
+		jl newMin
+		
+	end:
+		popq %rbx
+		ret
