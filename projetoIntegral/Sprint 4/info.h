2441a901dec4c9cb550f6afc4bfa4acf20a8d537
goncalor21 <1190610@isep.ipp.pt>
2022-01-12 00:23:24 +0000

    added file to import and adapted structure

--- projetoIntegral/Sprint 4/info.h
+++ projetoIntegral/Sprint 4/info.h
@@ -3,8 +3,18 @@
 
 
 typedef struct {
-	int id;
-	char name[50];
+	int x,y,z;
+	int container_number;
+	int check_digit;
+	float container_payload;
+	float container_tare;
+	float container_gross;
+	float container_volume;
+	char[4] iso_code;
+	char *certificates;
+	char *repairInfo;
+	char *type;
+	char *load;
 	int test;
 } Containers;
 
